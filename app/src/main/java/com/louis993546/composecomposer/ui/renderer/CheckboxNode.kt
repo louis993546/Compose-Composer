eff1113a2ddb1154f6e3aa11372e2cd54f6532f9
Louis Tsai <louis993546@gmail.com>
2021-05-22 22:24:19 +0200

    Add a bunch of previews and split a bunch of stuff

---
+++ app/src/main/java/com/louis993546/composecomposer/ui/renderer/CheckboxNode.kt
@@ -0,0 +1,28 @@
+package com.louis993546.composecomposer.ui.renderer
+
+import androidx.compose.material.Checkbox
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.louis993546.composecomposer.Node
+import com.louis993546.composecomposer.util.PreviewTheme
+
+@Composable
+fun CheckboxNode(
+    node: Node.Checkbox,
+) {
+    Checkbox(
+        checked = node.checked,
+        onCheckedChange = { TODO("need to modify this node, probably by propagate it back up") },
+    )
+}
+
+@Preview
+@Composable
+fun Preview_CheckboxNode() {
+    PreviewTheme {
+        CheckboxNode(node = Node.Checkbox(
+            text = "checkbox",
+            checked = true,
+        ))
+    }
+}
--- app/src/main/java/com/louis993546/composecomposer/util/Util.kt
+++ app/src/main/java/com/louis993546/composecomposer/util/Util.kt
@@ -1,6 +1,8 @@
 package com.louis993546.composecomposer.util
 
+import androidx.compose.runtime.Composable
 import com.louis993546.composecomposer.Id
+import com.louis993546.composecomposer.ui.theme.ComposeComposerTheme
 import kotlin.random.Random
 
 // TODO it'd be nice i can make sure it never collide
@@ -8,3 +10,11 @@ fun randId(): Id = Random.nextInt()
 
 val <T> T.exhaustive: T
     get() = this
+
+/**
+ * TODO see what makes sense for a Preview Theme
+ */
+@Composable
+fun PreviewTheme(content: @Composable () -> Unit,) {
+    ComposeComposerTheme(content = content)
+}
